a6b52a7a107cc43baaa64d0be248bd513872ebec
Anton Ryabenko <rjabenkoae@mail.ru>
2019-06-06 17:30:15 +0300

    Add files via upload

---
+++ vector.py
@@ -0,0 +1,31 @@
+from math import sqrt
+
+
+class Vector:
+	def __init__(self, coords):
+		self.coords = coords
+	def __float__(self):
+		return sqrt(sum([c**2 for c in self.coords]))
+	def __add__(self, other):
+		return Vector([(c1 + c2) for c1, c2 in zip(self.coords, other.coords)])
+	def __sub__(self, other):
+		return Vector([(c1 - c2) for c1, c2 in zip(self.coords, other.coords)])	
+	def __str__(self):
+		return '({})'.format(', '.join([str(c) for c in self.coords]))
+	def mult_k(self, k):
+		return Vector([k * c for c in self.coords])	
+
+
+if __name__ == '__main__':
+	a = Vector([2, 3, 4])
+	ma = float(a)
+	print(f'|a| = {ma:.2f}')
+	b = Vector([5, 4, 1])
+	mb = float(b)
+	print(f'|b| = {mb:.2f}')
+	c = a + b
+	print(c)
+	d = a - b
+	print(d)
+	e = a.mult_k(2.5)
+	print(e)
