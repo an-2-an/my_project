3732d95e404beb075443ffbe76551f0d1c166ed6
Anton Ryabenko <rjabenkoae@mail.ru>
2019-06-07 10:46:38 +0300

    Add files via upload

--- try_test.py
+++ try_test.py
@@ -18,30 +18,43 @@ class TestFunctions(unittest.TestCase):
             ]
 
     def test1(self):
+        '''Тестируем суммирование элементов списка'''
         for d in self.data1:
             self.assertEqual(get_sum(*d['params']), d['target'])
 
     def test2(self):
+        '''Тестируем проверку на четность'''
         self.assertTrue(is_even(10))
         self.assertFalse(is_even(7))
+        self.assertEqual(is_even(20), True)
         with self.assertRaises(TypeError):
             is_even('yyy')
 
     def test3(self):
+        '''Тестируем генератор списка'''
         self.assertEqual(get_list_from_a_to_b(1, 4), [1, 2, 3, 4])
         self.assertEqual(get_list_from_a_to_b(8, 4), [4, 5, 6, 7, 8])
 
     @unittest.skip('demo skipping')
     def test_none(self):
+        '''Пропускаем тест'''
         pass
 
     @unittest.skipIf((sys.platform.startswith('win')), 'not for windows')
     def test_if(self):
+        '''Пропускаем тест с условием'''
         pass
 
-    def test_fail(self):
+    @unittest.expectedFailure
+    def test_fail_1(self):
+        '''Ждем фейла и радуемся'''
+        self.assertEqual(is_even(2), False)
+
+    def test_fail_2(self):
+        '''Имитируем фейл'''
         self.fail('oooops...')
 
+# python -m unittest -v test_module
 if __name__ == '__main__':
     unittest.main(verbosity=2)
 
